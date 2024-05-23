4a6b7863785cf0ff8ea8e4f9aa142351b5676db8
Ashish <ashish@simplificommerce.com>
2024-05-23 15:06:44 +0530

    1. Added Routers for the application. 2. Created terms and condition and privacy policy pages 3. Modularised home page screen

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from "../components/Navbar";
+import Hero from "../components/HeroSection/Hero";
+import InfoSection from "../components/InfoSection/InfoSection";
+import AboutSection from "../components/AboutSection/AboutSection";
+import Features from "../components/FeaturesSection/Features";
+import Pricing from "../components/PricingSection/Pricing";
+import Faqs from "../components/Faqs/Faqs";
+import Footer from "../components/Footer";
+const Home = () => {
+  return (
+    <div className="overflow-hidden">
+			<Navbar />
+			<Hero />
+			<div className="px-[6rem]">
+				<InfoSection />
+				<AboutSection />
+				<Features />
+				<Pricing />
+				<Faqs />
+			</div>
+			<Footer />
+		</div>
+  );
+};
+
+export default Home;
